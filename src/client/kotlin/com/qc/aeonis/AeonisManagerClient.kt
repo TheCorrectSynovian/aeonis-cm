8c2f23bbf214de389fac4ad055e08f8d3250af7d
QuantumCreeper <prasanavenkatesh2007pv@gmail.com>
2025-12-25 17:18:29 +0530

    first commit

---
+++ src/client/kotlin/com/qc/aeonis/AeonisManagerClient.kt
@@ -0,0 +1,9 @@
+package com.qc.aeonis
+
+import net.fabricmc.api.ClientModInitializer
+
+object AeonisManagerClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/com/qc/aeonis/AeonisManagerDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.qc.aeonis
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object AeonisManagerDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
